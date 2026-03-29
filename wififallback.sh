31f0e94556c22848d544007638044b4a5b7b53da
Jimvdb <jvdb@trafiroad.be>
2026-03-29 20:05:17 +0200

    netwerk config stap 1

---
+++ wififallback.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+WIFI_NAME="preconfigured"
+AP_NAME="logger-ap"
+
+# check internet (of gateway)
+ping -c 2 8.8.8.8 > /dev/null
+
+if [ $? -ne 0 ]; then
+    echo "Geen wifi → AP starten"
+    nmcli con down "$WIFI_NAME"
+    nmcli con up "$AP_NAME"
+else
+    echo "Wifi OK"
+    nmcli con down "$AP_NAME" 2>/dev/null
+    nmcli con up "$WIFI_NAME"
+fi
